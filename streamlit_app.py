c215bbdeda9421ea8dd7ee459c9bff14cf5c4534
polomint82 <polomint82@users.noreply.github.com>
2023-07-04 22:27:24 -0400

    Update streamlit_app.py
    Setting up the Menu items.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("Independent Research and Certification for Snowflake Exam")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
