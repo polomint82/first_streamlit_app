7ac4adc465cf564c79b151a364f37b11342dfe4f
polomint82 <polomint82@users.noreply.github.com>
2023-07-04 22:40:40 -0400

    Update streamlit_app.py
    Removed the stupid Emoji's. They're distracting.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,12 @@ import pandas
 streamlit.title("Independent Research and Certification for Snowflake Exam")
 
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🍇🥝')
+streamlit.header('Build Your Own Fruit Smoothie')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
